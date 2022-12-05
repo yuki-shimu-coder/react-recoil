5f54ee4473d72640acbd760ee47f0ceb1ecbbda2
github yuki-shimu-coder <yuyuyou7@gmail.com>
2022-12-05 23:54:11 +0900

    テキストのstate及びset関数を実装

---
+++ reactApp/react-sample/src/screen/TopScreen.jsx
@@ -0,0 +1,17 @@
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { textState } from '../store/textState';
+
+
+export const TopScreen = () => {
+  const [text, setText] = useRecoilState(textState);
+  const onChange = (event) => {
+    setText(event.target.value)
+  }
+  return (
+    <>
+      <h1>TopScreen</h1>
+      <p>Stateの値：{text}</p>
+      <input type="text" onChange={onChange} />
+    </>
+  );
+}
