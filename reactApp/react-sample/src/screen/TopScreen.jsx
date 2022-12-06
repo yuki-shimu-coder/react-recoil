b692412df95cf2b03b99a0e51c731063531e4b78
github yuki-shimu-coder <yuyuyou7@gmail.com>
2022-12-06 23:28:20 +0900

    カウンターを実装

--- reactApp/react-sample/src/screen/TopScreen.jsx
+++ reactApp/react-sample/src/screen/TopScreen.jsx
@@ -1,17 +1,25 @@
 import { useRecoilState } from 'recoil';
+import { countState } from '../store/countState';
 import { textState } from '../store/textState';
 
 
 export const TopScreen = () => {
+  /** textState */
   const [text, setText] = useRecoilState(textState);
   const onChange = (event) => {
     setText(event.target.value)
   }
+  /** countState */
+  const [count, setCount] = useRecoilState(countState)
+  const onClick = () => { setCount(count + 1) }
   return (
     <>
       <h1>TopScreen</h1>
       <p>Stateの値：{text}</p>
       <input type="text" onChange={onChange} />
+      <br />
+      <p>count：{count}</p>
+      <button onClick={onClick}>count++</button>
     </>
   );
 }
